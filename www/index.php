1bdf8d5393a7b77be0e9af12d6189b6309a28f7b
James Muspratt <james@jamesmuspratt.com>
2020-07-14 14:01:53 -0400

    Index cleanup

--- www/index.php
+++ www/index.php
@@ -5,7 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="app/css/style.css"/>
         <title>Photos</title>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png">
     </head>
     <body>
 
@@ -15,16 +14,14 @@ error_reporting(E_ALL);
 include 'app/config.php';
 include 'app/functions.php';
 
-
 $album = isset($_GET['a']) ? $_GET['a'] : null;
 
 if ($album) :
         include 'app/views/view-album.php';
 else :
         include 'app/views/view-index.php';
-    ?>
+endif;
+?>
 
-
-<?php endif; ?>
 </body>
 </html>
