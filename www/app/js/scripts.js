934890109403a6ee1c68d05a1c8508df2624dd93
James Muspratt <james@jamesmuspratt.com>
2020-10-07 09:00:20 -0400

    JS comments

--- www/app/js/scripts.js
+++ www/app/js/scripts.js
@@ -4,26 +4,28 @@ document.addEventListener("DOMContentLoaded", function() {
   const albumNavCurtain = document.querySelector('.js-album-nav-curtain');
   const albumNavToggle = document.querySelector('.js-album-nav-toggle');
 
+  // Clicking btn toggles album nav
   albumNavToggle.addEventListener('click', (e) => {
     body.classList.toggle('js-album-nav-shown');
   });
 
+  // Clicking background "curtain" closes album nav
   albumNavCurtain.addEventListener('click', (e) => {
     body.classList.remove('js-album-nav-shown');
   });
 
-
+  // Pressing escape key closes album nav
   document.onkeydown = function(evt) {
     evt = evt || window.event;
-    var isEscape = false;
-    if ("key" in evt) {
-        isEscape = (evt.key === "Escape" || evt.key === "Esc");
+    let isEscape = false;
+    if ('key' in evt) {
+      isEscape = (evt.key === "Escape" || evt.key === "Esc");
     } else {
-        isEscape = (evt.keyCode === 27);
+      isEscape = (evt.keyCode === 27);
     }
     if (isEscape) {
       body.classList.remove('js-album-nav-shown');
     }
-};
+  };
 
 });
