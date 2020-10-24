217352f24d32db8ee042f6cb1a2511d6cc368375
James Muspratt <james@jamesmuspratt.com>
2020-10-24 13:31:42 -0400

    Typo

--- www/app/config.example.php
+++ www/app/config.example.php
@@ -26,5 +26,5 @@ define("THUMB_SIZES", [ 900, 1800, 2700, 3600 ]);
 define("THUMB_LARGEST", "3600");
 
 // Height of videos to be generated
-//(source aspect ratio will bepreserved)
+//(source aspect ratio will be preserved)
 define("VIDEO_HEIGHT", 720);
