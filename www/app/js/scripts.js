852f476f6ec9c78d671334155c5e90c011d25217
James <james@family-air.lan>
2020-12-21 11:12:56 -0500

    Begin

--- www/app/js/scripts.js
+++ www/app/js/scripts.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
   const body = document.querySelector('body');
   const albumNavCurtain = document.querySelector('.js-album-nav-curtain');
   const albumNavToggle = document.querySelector('.js-album-nav-toggle');
+  const lightboxTriggers = document.querySelectorAll('.js-lightbox-trigger');
 
   // Clicking btn toggles album nav
   albumNavToggle.addEventListener('click', (e) => {
@@ -27,5 +28,29 @@ document.addEventListener("DOMContentLoaded", function() {
       body.classList.remove('js-album-nav-shown');
     }
   };
+
+  // Click an image/video to expand
+  lightboxTriggers.forEach(item=>{
+    item.addEventListener('click', (e) => {
+      e.preventDefault();
+      const target = e.target;
+      console.log(target.tagName);
+
+      if (target.tagName == 'IMG') {
+      }
+
+      if (target.tagName == 'VIDEO') {
+        const source = target.querySelector('source').getAttribute('src');
+
+        const markup = `
+        <video controls autoplay>
+          <source src="${source}" />
+        </video>
+        `;
+        console.log(markup);
+      }
+
+    });
+  });
 
 });
